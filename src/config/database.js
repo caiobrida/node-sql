1fc18f59d259c75ecf655dea91d3dfa3ba910225
ayax <caiobrida1@gmail.com>
2020-02-05 05:44:13 -0300

    only commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'rajje.db.elephantsql.com',
+    port: 5432,
+    username: 'sgrvhmts',
+    password: 'zy0gnc9PW9WIltkSmmTVubJhNszS14Pr',
+    database: 'sgrvhmts',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+}
